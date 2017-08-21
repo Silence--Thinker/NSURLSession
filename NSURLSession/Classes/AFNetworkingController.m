ecb15fa7dc72aa5fee407a328ceb88b54841c013
silence <154261688@qq.com>
2017-08-21 08:07:26 +0800

    提交

--- NSURLSession/Classes/AFNetworkingController.m
+++ NSURLSession/Classes/AFNetworkingController.m
@@ -8,7 +8,7 @@
 
 #import "AFNetworkingController.h"
 
-@interface AFNetworkingController ()<NSURLSessionDelegate>
+@interface AFNetworkingController ()<NSURLSessionDelegate,NSURLSessionTaskDelegate>
 
 @end
 @implementation AFNetworkingController
@@ -27,7 +27,7 @@
     NSURLSession *session = [NSURLSession sessionWithConfiguration:sessionConfig delegate:self delegateQueue:queue];
     
     
-    NSURL *url = [NSURL URLWithString:@"https://tcc.taobao.com/cc/json/mobile_tel_segment.htm?tel=18755057919"];
+    NSURL *url = [NSURL URLWithString:@"http://www.kuaidi100.com/query?type=yuantong&postid=11111111111"];
     NSMutableURLRequest *request = [[NSMutableURLRequest alloc] initWithURL:url];
     request.HTTPMethod = @"GET";
 //    request.m
@@ -39,6 +39,34 @@
     [dataTask resume];
 }
 
+// MARK - NSURLSessionTaskDelegate
+
+- (void)URLSession:(NSURLSession *)session dataTask:(NSURLSessionDataTask *)dataTask
+didReceiveResponse:(NSURLResponse *)response
+ completionHandler:(void (^)(NSURLSessionResponseDisposition disposition))completionHandler {
+    NSLog(@"%s", __func__);
+    completionHandler(NSURLSessionResponseAllow);
+}
+
+- (void)URLSession:(NSURLSession *)session dataTask:(NSURLSessionDataTask *)dataTask didReceiveData:(NSData *)data {
+    NSLog(@"%s", __func__);
+    NSError *error;
+    NSDictionary *dict = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:&error];
+    NSLog(@" dict=>> %@",dict);
+}
+
+- (void)URLSession:(NSURLSession *)session task:(NSURLSessionTask *)task didCompleteWithError:(NSError *)error {
+    NSLog(@"%s", __func__);
+    if(error == nil) {
+        NSLog(@"Download is Succesfull");
+    }
+    else{
+        NSLog(@"Error %@",[error userInfo]);
+    }
+}
+
+/*
+
 // MARK - NSURLSessionDelegate
 - (void)URLSession:(NSURLSession *)session didBecomeInvalidWithError:(nullable NSError *)error {
     NSLog(@"%s", __func__);
@@ -47,7 +75,7 @@
 - (void)URLSession:(NSURLSession *)session didReceiveChallenge:(NSURLAuthenticationChallenge *)challenge
  completionHandler:(void (^)(NSURLSessionAuthChallengeDisposition disposition, NSURLCredential * _Nullable credential))completionHandler {
     
-    
+//    completionHandler(NSURLSessionResponseAllow)
     NSLog(@"%@", session);
     NSLog(@"%s", __func__);
 }
@@ -55,5 +83,8 @@
 - (void)URLSessionDidFinishEventsForBackgroundURLSession:(NSURLSession *)sessio {
     NSLog(@"%s", __func__);
 }
+
+
+*/
 @end
 
